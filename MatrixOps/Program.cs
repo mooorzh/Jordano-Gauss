34e10665b348d0a95f40fbc7da1f86ab18fe1324
Дмитрий Голубков < >
2017-03-23 06:59:10 +0300

    My First GitGub probe

---
+++ MatrixOps/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] A = new double[3][];
+            A[0] = new double[4] { 5732, 2134, 2134, 7866 };
+            A[1] = new double[4] { 2134, 5732, 2134, 670 };
+            A[2] = new double[4] { 2134, 2134, 5732, 11464 };
+
+            SLAE test = new SLAE(A);
+            Console.WriteLine("Изначальная матрица:");
+            foreach (double[] i in test.PRIM)
+            {
+                string res = "";
+                foreach (double x in i)
+                    res += x.ToString() + " ";
+                Console.WriteLine(res);
+            }
+            Console.WriteLine("Преобразованная матрица:");
+            test.JordanoGauss();
+            foreach (double[] i in test.PRIM)
+            {
+                string res = "";
+                foreach (double x in i)
+                    res += x.ToString() + " ";
+                Console.WriteLine(res);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    class SLAE
+    {
+        public double[][] PRIM;
+        public SLAE(double[][] A)
+        {
+            PRIM = new double[A.Length][];
+            for (int i = 0; i < A.Length; i++)
+            {
+                PRIM[i] = new double[A[i].Length];
+                for (int j = 0; j < A[i].Length; j++)
+                    PRIM[i][j] = A[i][j];
+            }            
+        }
+        public void JordanoGauss()
+        {
+            double[][] A = PRIM;
+            for (int i = 0; (i < A.Length) && (i < A[0].Length); i++)
+            {
+
+                double tmp = A[i][i];
+                if (tmp >= 0.00000001)
+                {
+                    for (int j = i; j < A[0].Length; j++)
+                        A[i][j] /= tmp;
+                }
+                for (int j = 0; j < A.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        double tmp2 = A[j][i];
+                        for (int k = i; k < A[0].Length; k++)
+                            A[j][k] -= A[i][k] * tmp2;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MatrixOps/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MatrixOps")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatrixOps")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d486bb6a-a54e-4196-ba59-615a431dda9c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
